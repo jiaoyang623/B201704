bad3d90c6517a6ee282da330afa968dd41b0a697
jiaoyang623 <jiaoyang623@qq.com>
2017-06-19 09:49:11 +0800

    add application

--- Whisky/app/src/main/java/guru/ioio/whisky/PannelActivity.java
+++ Whisky/app/src/main/java/guru/ioio/whisky/PannelActivity.java
@@ -4,11 +4,18 @@ import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.text.Selection;
+import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.TextPaint;
+import android.text.method.BaseMovementMethod;
+import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 
 import guru.ioio.whisky.base.Utils;
 import guru.ioio.whisky.databinding.ActivityPannelBinding;
@@ -45,5 +52,47 @@ public class PannelActivity extends Activity {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_pannel);
         mBinding.setPresenter(this);
+
+        mBinding.spanText.setMovementMethod(new BaseMovementMethod(){
+            @Override
+            public boolean onTouchEvent(TextView widget, Spannable buffer,
+                                        MotionEvent event) {
+                int action = event.getAction();
+
+                if (action == MotionEvent.ACTION_UP ||
+                        action == MotionEvent.ACTION_DOWN) {
+                    int x = (int) event.getX();
+                    int y = (int) event.getY();
+
+                    x -= widget.getTotalPaddingLeft();
+                    y -= widget.getTotalPaddingTop();
+
+                    x += widget.getScrollX();
+                    y += widget.getScrollY();
+
+                    Layout layout = widget.getLayout();
+                    int line = layout.getLineForVertical(y);
+                    int off = layout.getOffsetForHorizontal(line, x);
+
+                    ClickableSpan[] link = buffer.getSpans(off, off, ClickableSpan.class);
+
+                    if (link.length != 0) {
+                        if (action == MotionEvent.ACTION_UP) {
+                            link[0].onClick(widget);
+                        } else if (action == MotionEvent.ACTION_DOWN) {
+                            Selection.setSelection(buffer,
+                                    buffer.getSpanStart(link[0]),
+                                    buffer.getSpanEnd(link[0]));
+                        }
+
+                        return true;
+                    } else {
+                        Selection.removeSelection(buffer);
+                    }
+                }
+
+                return super.onTouchEvent(widget, buffer, event);
+            }
+        });
     }
 }
---
+++ Whisky/app/src/main/java/guru/ioio/whisky/WebActivity.java
@@ -0,0 +1,10 @@
+package guru.ioio.whisky;
+
+import android.app.Activity;
+
+/**
+ * Created by daniel on 6/16/17.
+ */
+
+public class WebActivity extends Activity {
+}
