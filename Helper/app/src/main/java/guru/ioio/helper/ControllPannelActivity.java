551a98d0df9ff3c5fb09b7459246af1a84f7bf28
jiaoyang623 <jiaoyang623@qq.com>
2017-07-12 18:32:44 +0800

    add AudioHelper

---
+++ Helper/app/src/main/java/guru/ioio/helper/ControllPannelActivity.java
@@ -0,0 +1,18 @@
+package guru.ioio.helper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by daniel on 7/12/17.
+ */
+
+public class ControllPannelActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
---
+++ Helper/app/src/main/java/guru/ioio/helper/model/AudioHelper.java
@@ -0,0 +1,8 @@
+package guru.ioio.helper.model;
+
+/**
+ * Created by daniel on 7/12/17.
+ */
+
+public class AudioHelper {
+}
