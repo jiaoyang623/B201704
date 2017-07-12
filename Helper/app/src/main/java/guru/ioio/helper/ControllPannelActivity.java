ed10371b374982bd4eeeb9538bbc425542126671
jiaoyang623 <jiaoyang623@qq.com>
2017-07-12 19:27:13 +0800

    添加音量控制

--- Helper/app/src/main/java/guru/ioio/helper/ControllPannelActivity.java
+++ Helper/app/src/main/java/guru/ioio/helper/ControllPannelActivity.java
@@ -1,17 +1,28 @@
 package guru.ioio.helper;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import guru.ioio.helper.databinding.ActivityControlPannelBinding;
+import guru.ioio.helper.model.AudioHelper;
+
 /**
  * Created by daniel on 7/12/17.
+ * for changing settings
  */
 
 public class ControllPannelActivity extends Activity {
+    public AudioHelper audioHelper;
+    private ActivityControlPannelBinding mBinding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        audioHelper = new AudioHelper(this);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_control_pannel);
+        mBinding.setPresenter(this);
     }
 
 
--- Helper/app/src/main/java/guru/ioio/helper/model/AudioHelper.java
+++ Helper/app/src/main/java/guru/ioio/helper/model/AudioHelper.java
@@ -1,8 +1,37 @@
 package guru.ioio.helper.model;
 
+import android.content.Context;
+import android.media.AudioManager;
+
 /**
  * Created by daniel on 7/12/17.
  */
 
 public class AudioHelper {
+    private AudioManager mAudioManager;
+
+    public AudioHelper(Context context) {
+        //音量控制,初始化定义
+        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    }
+
+    public int getMaxVolume() {
+        return mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+    }
+
+
+    public int getVolume() {
+        return mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+    }
+
+    public void setVolume(int volume) {
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+    }
+
+    public boolean getMute() {
+        return false;
+    }
+
+    public void setMute(boolean isMute) {
+    }
 }
