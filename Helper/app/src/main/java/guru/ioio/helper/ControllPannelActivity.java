f7e0534793c5cb915c06f460979bc1f3930e149b
jiaoyang623 <jiaoyang623@qq.com>
2017-07-13 09:54:50 +0800

    添加注册监听

--- Helper/app/src/main/java/guru/ioio/helper/ControllPannelActivity.java
+++ Helper/app/src/main/java/guru/ioio/helper/ControllPannelActivity.java
@@ -1,6 +1,10 @@
 package guru.ioio.helper;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -16,14 +20,32 @@ import guru.ioio.helper.model.AudioHelper;
 public class ControllPannelActivity extends Activity {
     public AudioHelper audioHelper;
     private ActivityControlPannelBinding mBinding;
+    private BroadcastReceiver mVolumnChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (mBinding != null) {
+                mBinding.setPresenter(ControllPannelActivity.this);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         audioHelper = new AudioHelper(this);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_control_pannel);
-        mBinding.setPresenter(this);
+        registerReceiver(mVolumnChangeReceiver, new IntentFilter("android.media.VOLUME_CHANGED_ACTION"));
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mVolumnChangeReceiver);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mBinding.setPresenter(this);
+    }
 }
